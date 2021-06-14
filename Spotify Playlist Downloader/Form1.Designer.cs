416c27caa144a10e1180d91fe512c1d7cc54fc23
Henry Richard J <henry.richard.2k@gmail.com>
2021-06-14 09:32:02 +0530

    Uploading Files

---
+++ Spotify Playlist Downloader/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Spotify_Playlist_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonGetSongs = new System.Windows.Forms.Button();
+            this.textBox_PlaylistID = new System.Windows.Forms.TextBox();
+            this.listView_SongsList = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.donateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.paypalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.donateToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.paypalToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.developedByHenryRichardJToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.socialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.youtubeChannelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.telegramChannelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.buttonGetSongs);
+            this.groupBox1.Controls.Add(this.textBox_PlaylistID);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(992, 88);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Playlist ID";
+            // 
+            // buttonGetSongs
+            // 
+            this.buttonGetSongs.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonGetSongs.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGetSongs.Location = new System.Drawing.Point(837, 14);
+            this.buttonGetSongs.Name = "buttonGetSongs";
+            this.buttonGetSongs.Size = new System.Drawing.Size(144, 63);
+            this.buttonGetSongs.TabIndex = 1;
+            this.buttonGetSongs.Text = "Get Songs";
+            this.buttonGetSongs.UseVisualStyleBackColor = true;
+            this.buttonGetSongs.Click += new System.EventHandler(this.buttonGetSongs_Click);
+            // 
+            // textBox_PlaylistID
+            // 
+            this.textBox_PlaylistID.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_PlaylistID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_PlaylistID.Location = new System.Drawing.Point(6, 36);
+            this.textBox_PlaylistID.Name = "textBox_PlaylistID";
+            this.textBox_PlaylistID.Size = new System.Drawing.Size(809, 24);
+            this.textBox_PlaylistID.TabIndex = 0;
+            // 
+            // listView_SongsList
+            // 
+            this.listView_SongsList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView_SongsList.HideSelection = false;
+            this.listView_SongsList.Location = new System.Drawing.Point(0, 125);
+            this.listView_SongsList.Name = "listView_SongsList";
+            this.listView_SongsList.Size = new System.Drawing.Size(992, 559);
+            this.listView_SongsList.TabIndex = 1;
+            this.listView_SongsList.UseCompatibleStateImageBehavior = false;
+            this.listView_SongsList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listView_SongsList_MouseClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.donateToolStripMenuItem,
+            this.socialToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(992, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // donateToolStripMenuItem
+            // 
+            this.donateToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paypalToolStripMenuItem,
+            this.donateToolStripMenuItem1,
+            this.developedByHenryRichardJToolStripMenuItem});
+            this.donateToolStripMenuItem.Name = "donateToolStripMenuItem";
+            this.donateToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.donateToolStripMenuItem.Text = "About";
+            this.donateToolStripMenuItem.Click += new System.EventHandler(this.donateToolStripMenuItem_Click);
+            // 
+            // paypalToolStripMenuItem
+            // 
+            this.paypalToolStripMenuItem.Name = "paypalToolStripMenuItem";
+            this.paypalToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.paypalToolStripMenuItem.Text = "Github";
+            this.paypalToolStripMenuItem.Click += new System.EventHandler(this.paypalToolStripMenuItem_Click);
+            // 
+            // donateToolStripMenuItem1
+            // 
+            this.donateToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.paypalToolStripMenuItem1});
+            this.donateToolStripMenuItem1.Name = "donateToolStripMenuItem1";
+            this.donateToolStripMenuItem1.Size = new System.Drawing.Size(291, 26);
+            this.donateToolStripMenuItem1.Text = "Donate";
+            // 
+            // paypalToolStripMenuItem1
+            // 
+            this.paypalToolStripMenuItem1.Name = "paypalToolStripMenuItem1";
+            this.paypalToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.paypalToolStripMenuItem1.Text = "Paypal";
+            this.paypalToolStripMenuItem1.Click += new System.EventHandler(this.paypalToolStripMenuItem1_Click);
+            // 
+            // developedByHenryRichardJToolStripMenuItem
+            // 
+            this.developedByHenryRichardJToolStripMenuItem.Name = "developedByHenryRichardJToolStripMenuItem";
+            this.developedByHenryRichardJToolStripMenuItem.Size = new System.Drawing.Size(291, 26);
+            this.developedByHenryRichardJToolStripMenuItem.Text = "Developed By Henry Richard J";
+            // 
+            // socialToolStripMenuItem
+            // 
+            this.socialToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.youtubeChannelToolStripMenuItem,
+            this.telegramChannelToolStripMenuItem});
+            this.socialToolStripMenuItem.Name = "socialToolStripMenuItem";
+            this.socialToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.socialToolStripMenuItem.Text = "Social";
+            // 
+            // youtubeChannelToolStripMenuItem
+            // 
+            this.youtubeChannelToolStripMenuItem.Name = "youtubeChannelToolStripMenuItem";
+            this.youtubeChannelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.youtubeChannelToolStripMenuItem.Text = "Youtube Channel";
+            this.youtubeChannelToolStripMenuItem.Click += new System.EventHandler(this.youtubeChannelToolStripMenuItem_Click);
+            // 
+            // telegramChannelToolStripMenuItem
+            // 
+            this.telegramChannelToolStripMenuItem.Name = "telegramChannelToolStripMenuItem";
+            this.telegramChannelToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.telegramChannelToolStripMenuItem.Text = "Telegram Channel";
+            this.telegramChannelToolStripMenuItem.Click += new System.EventHandler(this.telegramChannelToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 684);
+            this.Controls.Add(this.listView_SongsList);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spotify Playlist Downloader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonGetSongs;
+        private System.Windows.Forms.TextBox textBox_PlaylistID;
+        private System.Windows.Forms.ListView listView_SongsList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem donateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem paypalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem donateToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem paypalToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem developedByHenryRichardJToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem socialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem youtubeChannelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem telegramChannelToolStripMenuItem;
+    }
+}
+
---
+++ Spotify Playlist Downloader/Form1.cs
@@ -0,0 +1,149 @@
+﻿using Leaf.xNet;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spotify_Playlist_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        JArray playlistArray;
+
+        private void buttonGetSongs_Click(object sender, EventArgs e)
+        {
+            listView_SongsList.Items.Clear();
+
+            ImageList dowloadedImages = new ImageList();
+            dowloadedImages.ImageSize = new Size(236, 236);
+            dowloadedImages.ColorDepth = ColorDepth.Depth32Bit;
+
+            dowloadedImages.Images.Clear();
+            try
+            {
+                HttpRequest tokenRequest = new HttpRequest();
+                tokenRequest.UserAgent = Http.ChromeUserAgent();
+                String tokenJson = tokenRequest.Get("https://open.spotify.com/get_access_token?reason=transport&productType=web_player").ToString();
+
+                JObject spotifyJsonToken = JObject.Parse(tokenJson);
+
+                String spotifyToken = spotifyJsonToken.SelectToken("accessToken").ToString();
+
+                HttpRequest getSpotifyPlaylist = new HttpRequest();
+                getSpotifyPlaylist.AddHeader("Authorization", "Bearer " + spotifyToken);
+                String playlist = getSpotifyPlaylist.Get("https://api.spotify.com/v1/playlists/" + textBox_PlaylistID.Text + "/tracks?offset=0&limit=100").ToString();
+
+                JObject jobject = JObject.Parse(playlist);
+
+                playlistArray = JArray.Parse(jobject.SelectToken("items").ToString());
+
+                for (int i = 0; i < playlistArray.Count; i++)
+                {
+                    HttpRequest w = new HttpRequest();
+                    listView_SongsList.Items.Add(playlistArray[i].SelectToken("track").SelectToken("name").ToString(), i);
+                    try
+                    {
+                        var response = w.Get(playlistArray[i].SelectToken("track").SelectToken("album").SelectToken("images")[0].SelectToken("url").ToString());
+                        byte[] imageBytes = response.ToBytes();
+                        MemoryStream memoryStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                        memoryStream.Write(imageBytes, 0, imageBytes.Length);
+                        Image imgs = Image.FromStream(memoryStream, true);
+                        dowloadedImages.Images.Add(imgs);
+                    }
+                    catch
+                    {
+                        var response = w.Get("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png");
+                        byte[] imageBytes = response.ToBytes();
+                        MemoryStream memoryStream = new MemoryStream(imageBytes, 0, imageBytes.Length);
+                        memoryStream.Write(imageBytes, 0, imageBytes.Length);
+                        Image imgs = Image.FromStream(memoryStream, true);
+                        dowloadedImages.Images.Add(imgs);
+                    }
+
+
+                }
+                listView_SongsList.LargeImageList = dowloadedImages;
+            }
+            catch
+            {
+                MessageBox.Show("Make Sure You Have passed Playlist ID and Not URL", "Playlist Not Found ERROR");
+            }
+
+
+        }
+
+        private void listView_SongsList_MouseClick(object sender, MouseEventArgs e)
+        {
+            var downloadPath = Environment.CurrentDirectory + @"\\Downloads";
+            var client = new WebClient();
+            client.DownloadFile(playlistArray[listView_SongsList.FocusedItem.Index].SelectToken("track").SelectToken("album").SelectToken("images")[0].SelectToken("url").ToString(),downloadPath+"/thumb.jpg");
+
+            string songName = playlistArray[listView_SongsList.FocusedItem.Index].SelectToken("track").SelectToken("name").ToString();
+            string artists = playlistArray[listView_SongsList.FocusedItem.Index].SelectToken("track").SelectToken("artists")[0].SelectToken("name").ToString();
+            string songAlbum = playlistArray[listView_SongsList.FocusedItem.Index].SelectToken("track").SelectToken("album").SelectToken("name").ToString();
+            
+
+
+
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            process.StartInfo.FileName = Environment.CurrentDirectory + @"\\youtube-dl.exe";
+
+            
+            process.StartInfo.Arguments = "-x --no-continue " + "\"" + "ytsearch1: " + songName + " " + artists + "\" " + "--audio-format mp3 --audio-quality 0 -o " + "/Downloads/"+"\"" + songName + " - " + songAlbum +"\""+ "." + "%(ext)s";
+            process.Start();
+            process.WaitForExit();
+
+            System.Diagnostics.Process tagEditor = new System.Diagnostics.Process();
+            tagEditor.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            
+            tagEditor.StartInfo.FileName = Environment.CurrentDirectory + @"\\tageditor.exe";
+
+
+            tagEditor.StartInfo.Arguments = "set title=" + "\"" + songName + "\""+" album=" + "\"" + songAlbum + "\"" + " artist=" + "\"" + artists + "\"" + " cover=Downloads/thumb.jpg --files " + "\"" + "Downloads/" + songName + " - " + songAlbum + ".mp3"+"\"";
+            tagEditor.Start();
+            tagEditor.WaitForExit();
+
+            File.Delete(Path.Combine(downloadPath, songName + " - " + songAlbum + ".mp3.bak"));
+            File.Delete(Path.Combine(downloadPath, "thumb.jpg"));
+        }
+
+        private void paypalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/henry-richard7");
+        }
+
+        private void donateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void paypalToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.paypal.com/paypalme/henryrics");
+        }
+
+        private void youtubeChannelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.youtube.com/channel/UCVGasc5jr45eZUpZNHvbtWQ");
+        }
+
+        private void telegramChannelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://t.me/cracked4free");
+        }
+    }
+}
